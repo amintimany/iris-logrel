56cbb5ac9a8994182854c7b19f35e176c2cd18a7
Amin Timany <amintimany@gmail.com>
2016-05-21 12:54:09 +0200

    Prove type soundness for Fμ

---
+++ F_mu/soundness.v
@@ -0,0 +1,54 @@
+Require Import iris.proofmode.tactics.
+Require Import iris.program_logic.hoare.
+Require Import iris.program_logic.lifting.
+Require Import iris.algebra.upred_big_op.
+Require Import F_mu.lang F_mu.typing F_mu.rules F_mu.logrel F_mu.fundamental.
+Require Import iris.program_logic.adequacy.
+Import uPred.
+
+Section Soundness.
+  Context {Σ : iFunctor}.
+
+  Lemma empty_env_subst e : e.[env_subst []] = e.
+    replace (env_subst []) with (@ids expr _) by reflexivity.
+    asimpl; trivial.
+  Qed.
+
+  Definition free_type_context : leibniz_var -n> leibniz_val -n> iProp lang Σ :=
+    {|
+      cofe_mor_car :=
+        λ x,
+        {|
+          cofe_mor_car :=
+            λ y, (True)%I
+        |}
+    |}.
+
+  Global Instance free_context_interp_Persistent :
+    context_interp_Persistent free_type_context.
+  Proof. intros x v; apply const_persistent. Qed.
+
+  Lemma wp_soundness e τ
+    : typed [] e τ → True ⊢ WP e {{@interp Σ τ free_type_context}}.
+  Proof.
+    iIntros {H} "".
+    rewrite -(empty_env_subst e).
+    iPoseProof (@typed_interp _ _ _ []) as "Hi"; eauto; try typeclasses eauto.
+    iApply "Hi"; eauto.
+  Qed.
+
+  Theorem Soundness e τ :
+    typed [] e τ →
+    ∀ e' thp, rtc step ([e], tt) (e' :: thp, tt) →
+              ¬ reducible e' tt → is_Some (to_val e').
+  Proof.
+    intros H1 e' thp Hstp Hnr.
+    eapply wp_soundness in H1; eauto.
+    edestruct(@wp_adequacy_reducible lang Σ ⊤ (interp τ free_type_context)
+                                     e e' (e' :: thp) tt ∅) as [Ha|Ha];
+      eauto using cmra_unit_valid; try tauto.
+    - iIntros "H". iApply H1.
+    - constructor.
+  Qed.
+
+End Soundness.
